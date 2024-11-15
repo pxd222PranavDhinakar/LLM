efb391e4943926342990c285d9c5f1ef4b7ff085
pxd222PranavDhinakar <pxd222@case.edu>
2024-11-15 13:07:13 -0500

    Update

--- Experiments/Emergence_Testing/submit_job.sh
+++ Experiments/Emergence_Testing/submit_job.sh
@@ -7,16 +7,5 @@
 #SBATCH --mem=8gb
 #SBATCH --time=24:00:00
 
-# Load necessary modules
-module purge  # Clear any existing modules
-module load GCC/11.3.0
-module load CUDA/11.7.0
-module load PyTorch/2.0.1-foss-2022b-CUDA-11.7.0
-module load matplotlib/3.7.1
-module load Seaborn/0.12.2
-
-export CUDA_VISIBLE_DEVICES=0
-export TORCH_USE_CUDA_DSA=1
-
 # Run the script
 python emergence_analyzer.py
