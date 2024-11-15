b5e57aad11ba801f857bafe3dda801a4d469bffd
pxd222PranavDhinakar <pxd222@case.edu>
2024-11-14 23:06:47 -0500

    Update

--- Experiments/Emergence_Testing/submit_job.sh
+++ Experiments/Emergence_Testing/submit_job.sh
@@ -3,8 +3,7 @@
 #SBATCH --output=output_%j.log
 #SBATCH --error=error_%j.log
 #SBATCH --partition=markov_gpu
-#SBATCH --nodelist=classt02  # Specify the node with your RTX 2080 Ti
-#SBATCH --gres=gpu:RTX2080Ti:1  # Specifically request the RTX 2080 Ti
+#SBATCH --gres=gpu:1
 #SBATCH --mem=8gb
 #SBATCH --time=24:00:00
 
@@ -16,12 +15,6 @@ module load tqdm
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 module load matplotlib/3.7.2-gfbf-2023a
 module load Seaborn/0.13.2-gfbf-2023a
-
-# Ensure we're using the right GPU
-export CUDA_VISIBLE_DEVICES=0
-
-# Print GPU info
-nvidia-smi
 
 # Run the script
 python emergence_analyzer.py
